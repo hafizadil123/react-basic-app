e88562ec04ec774747194ddca1449ec33be5930f
Adil Sikandar <zeeshanhurmat@192.168.10.5>
2020-07-05 22:36:54 +0300

    change name

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React by adil sikandar
+          Learn React by saeed ahmead
         </a>
       </header>
     </div>
